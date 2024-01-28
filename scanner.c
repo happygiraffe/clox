1849548e90bc6b630183e0455072b03ec2299d7d
Dominic Mitchell <dom@happygiraffe.net>
2024-01-28 17:12:10 +0000

    16.3: flesh out the basic grammar

--- scanner.c
+++ scanner.c
@@ -19,6 +19,11 @@ void initScanner(const char *source)
     scanner.line = 1;
 }
 
+static bool isDigit(char c)
+{
+    return c >= '0' && c <= '9';
+}
+
 static bool isAtEnd()
 {
     return *scanner.current == '\0';
@@ -45,10 +50,137 @@ static Token errorToken(const char *message)
     return token;
 }
 
+static char advance()
+{
+    scanner.current++;
+    return scanner.current[-1];
+}
+
+static bool match(char expected)
+{
+    if (isAtEnd())
+        return false;
+    if (*scanner.current != expected)
+        return false;
+    scanner.current++;
+    return true;
+}
+
+static char peek()
+{
+    return *scanner.current;
+}
+
+static char peekNext()
+{
+    if (isAtEnd())
+        return '\0';
+    return scanner.current[1];
+}
+
+static void skipWhitespace()
+{
+    for (;;)
+    {
+        char c = peek();
+        switch (c)
+        {
+        case ' ':
+        case '\r':
+        case '\t':
+            advance();
+            break;
+
+        case '\n':
+            scanner.line++;
+            advance();
+            break;
+        case '/':
+            if (peekNext() != '/')
+                return;
+            // Ignore the rest of the comment, up to the newline (which is
+            // handled above, to ensure proper line number accounting).
+            while (peek() != '\n' && !isAtEnd())
+                advance();
+            break;
+        default:
+            return;
+        }
+    }
+}
+
+static Token number()
+{
+    while (isDigit(peek()))
+        advance();
+    // Look for a fractional part.
+    if (peek() == '.' && isDigit(peekNext()))
+    {
+        advance(); // consume the dot
+        while (isDigit(peek()))
+            advance();
+    }
+    return makeToken(TOKEN_NUMBER);
+}
+
+static Token string()
+{
+    while (peek() != '"' && !isAtEnd())
+    {
+        if (peek() == '\n')
+            scanner.line++;
+        advance();
+    }
+    if (isAtEnd())
+        return errorToken("Unterminated string.");
+    advance(); // consume the closing quote
+    return makeToken(TOKEN_STRING);
+}
+
 Token scanToken()
 {
+    skipWhitespace();
     scanner.start = scanner.current;
     if (isAtEnd())
         return makeToken(TOKEN_EOF);
+
+    char c = advance();
+    if (isDigit(c))
+        return number();
+    switch (c)
+    {
+    case '(':
+        return makeToken(TOKEN_LEFT_PAREN);
+    case ')':
+        return makeToken(TOKEN_RIGHT_PAREN);
+    case '{':
+        return makeToken(TOKEN_LEFT_BRACE);
+    case '}':
+        return makeToken(TOKEN_RIGHT_BRACE);
+    case ';':
+        return makeToken(TOKEN_SEMICOLON);
+    case ',':
+        return makeToken(TOKEN_COMMA);
+    case '.':
+        return makeToken(TOKEN_DOT);
+    case '-':
+        return makeToken(TOKEN_MINUS);
+    case '+':
+        return makeToken(TOKEN_PLUS);
+    case '/':
+        return makeToken(TOKEN_SLASH);
+    case '*':
+        return makeToken(TOKEN_STAR);
+    case '!':
+        return makeToken(match('=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
+    case '=':
+        return makeToken(match('=') ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL);
+    case '<':
+        return makeToken(match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
+    case '>':
+        return makeToken(match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+    case '"':
+        return string();
+    }
     return errorToken("Unexpected character.");
 }
