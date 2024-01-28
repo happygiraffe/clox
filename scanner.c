5d2841b736268def28dd399133d3148159a638f5
Dominic Mitchell <dom@happygiraffe.net>
2024-01-28 16:15:52 +0000

    16.2: initial scanner

--- scanner.c
+++ scanner.c
@@ -1,3 +1,5 @@
+#include <string.h>
+
 #include "common.h"
 #include "scanner.h"
 
@@ -15,4 +17,38 @@ void initScanner(const char *source)
     scanner.start = source;
     scanner.current = source;
     scanner.line = 1;
+}
+
+static bool isAtEnd()
+{
+    return *scanner.current == '\0';
+}
+
+static Token makeToken(TokenType type)
+{
+    Token token;
+    token.type = type;
+    token.start = scanner.start;
+    token.length = (int)(scanner.current - scanner.start);
+    token.line = scanner.line;
+    return token;
+}
+
+// message must outlive the error token. This is normally the case for static strings.
+static Token errorToken(const char *message)
+{
+    Token token;
+    token.type = TOKEN_ERROR;
+    token.start = message;
+    token.length = strlen(message);
+    token.line = scanner.line;
+    return token;
+}
+
+Token scanToken()
+{
+    scanner.start = scanner.current;
+    if (isAtEnd())
+        return makeToken(TOKEN_EOF);
+    return errorToken("Unexpected character.");
 }
