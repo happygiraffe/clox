0159aafcfdb8d5773aba7125ff4ee0b7fe10018a
Dominic Mitchell <dom@happygiraffe.net>
2024-02-24 17:41:08 +0000

    18.1-3: value types.
    (skipped temp broken states)

--- vm.c
+++ vm.c
@@ -3,6 +3,7 @@
 #include "common.h"
 #include "compiler.h"
 #include "debug.h"
+#include "stdarg.h"
 #include "vm.h"
 
 /* A single shared instance 😭 */
@@ -11,6 +12,20 @@ VM vm;
 static void resetStack()
 {
     vm.stackTop = vm.stack;
+}
+
+static void runtimeError(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    vfprintf(stderr, format, args);
+    fputs("\n", stderr);
+
+    // As we've already advance()d, we have to go back one.
+    size_t instruction = vm.ip - vm.chunk->code - 1;
+    int line = vm.chunk->lines[instruction];
+    fprintf(stderr, "[line %d] in script\n", line);
+    resetStack();
 }
 
 void initVM()
@@ -34,16 +49,26 @@ Value pop()
     return *vm.stackTop; /* underflow schmunderflow! 🤦 */
 }
 
+static Value peek(int distance)
+{
+    return vm.stackTop[-1 - distance];
+}
+
 /* static */ InterpretResult run()
 {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
-#define BINARY_OP(op)     \
-    do                    \
-    {                     \
-        double b = pop(); \
-        double a = pop(); \
-        push(a op b);     \
+#define BINARY_OP(valueType, op)                        \
+    do                                                  \
+    {                                                   \
+        if (!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))) \
+        {                                               \
+            runtimeError("Operands must be numbers.");  \
+            return INTERPRET_RUNTIME_ERROR;             \
+        }                                               \
+        double b = AS_NUMBER(pop());                    \
+        double a = AS_NUMBER(pop());                    \
+        push(valueType(a op b));                        \
     } while (false)
 
     for (;;)
@@ -69,19 +94,24 @@ Value pop()
             break;
         }
         case OP_ADD:
-            BINARY_OP(+);
+            BINARY_OP(NUMBER_VAL, +);
             break;
         case OP_SUBTRACT:
-            BINARY_OP(-);
+            BINARY_OP(NUMBER_VAL, -);
             break;
         case OP_MULTIPLY:
-            BINARY_OP(*);
+            BINARY_OP(NUMBER_VAL, *);
             break;
         case OP_DIVIDE:
-            BINARY_OP(/);
+            BINARY_OP(NUMBER_VAL, /);
             break;
         case OP_NEGATE:
-            push(-pop());
+            if (!IS_NUMBER(peek(0)))
+            {
+                runtimeError("Operand must be a number.");
+                return INTERPRET_RUNTIME_ERROR;
+            }
+            push(NUMBER_VAL(-AS_NUMBER(pop())));
             break;
         case OP_RETURN:
             printValue(pop());
@@ -99,7 +129,8 @@ InterpretResult interpret(const char *source)
 {
     Chunk chunk;
     initChunk(&chunk);
-    if (!compile(source, &chunk)) {
+    if (!compile(source, &chunk))
+    {
         freeChunk(&chunk);
         return INTERPRET_COMPILE_ERROR;
     }
