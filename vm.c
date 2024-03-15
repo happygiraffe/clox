6a082968c7c3618a1a11e75f44665bf1a9df69ab
Dominic Mitchell <dom@happygiraffe.net>
2024-03-15 23:49:38 +0000

    23.3: while loops

--- vm.c
+++ vm.c
@@ -247,6 +247,12 @@ static void concatenate()
                 vm.ip += offset;
             break;
         }
+        case OP_LOOP:
+        {
+            uint16_t offset = READ_SHORT();
+            vm.ip -= offset;
+            break;
+        }
         case OP_RETURN:
             // Exit interpreter.
             return INTERPRET_OK;
