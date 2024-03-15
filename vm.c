d776b649108e594294d8fceb85d9cd70795883bf
Dominic Mitchell <dom@happygiraffe.net>
2024-03-15 23:12:58 +0000

    23.1: jump around

--- vm.c
+++ vm.c
@@ -86,6 +86,8 @@ static void concatenate()
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 #define READ_STRING() AS_STRING(READ_CONSTANT())
+#define READ_SHORT() \
+    (vm.ip += 2, (uint16_t)(vm.ip[-2] << 8 | vm.ip[-1]))
 #define BINARY_OP(valueType, op)                        \
     do                                                  \
     {                                                   \
@@ -232,6 +234,19 @@ static void concatenate()
             printValue(pop());
             printf("\n");
             break;
+        case OP_JUMP:
+        {
+            uint16_t offset = READ_SHORT();
+            vm.ip += offset;
+            break;
+        }
+        case OP_JUMP_IF_FALSE:
+        {
+            uint16_t offset = READ_SHORT();
+            if (isFalsey(peek(0)))
+                vm.ip += offset;
+            break;
+        }
         case OP_RETURN:
             // Exit interpreter.
             return INTERPRET_OK;
@@ -242,6 +257,7 @@ static void concatenate()
 #undef READ_CONSTANT
 #undef READ_STRING
 #undef READ_BYTE
+#undef READ_SHORT
 }
 
 InterpretResult interpret(const char *source)
