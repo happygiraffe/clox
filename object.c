45f0bea20b785dbc7632458013cadec2b53c0701
Dominic Mitchell <dom@happygiraffe.net>
2024-10-06 19:58:29 +0100

    26.7: fix a GC bug in allocateString()

--- object.c
+++ object.c
@@ -58,7 +58,9 @@ static ObjString *allocateString(char *chars, int length, uint32_t hash)
     string->length = length;
     string->chars = chars;
     string->hash = hash;
+    push(OBJ_VAL(string)); // GC reachability
     tableSet(&vm.strings, string, NIL_VAL);
+    pop();
     return string;
 }
 
