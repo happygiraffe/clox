efbcd205e6f043039c05aadbdf6bb9fe0c14a172
Dominic Mitchell <dom@happygiraffe.net>
2024-10-13 16:16:05 +0100

    30.3 NaN boxing

--- common.h
+++ common.h
@@ -5,6 +5,9 @@
 #include <stddef.h>
 #include <stdint.h>
 
+/* When enabled, store values in a float64. */
+#define NAN_BOXING
+
 /* When defined, print chunks. */
 #define DEBUG_PRINT_CODE
 /* When defined, print each instruction prior to execution. */
--- value.h
+++ value.h
@@ -1,10 +1,61 @@
 #ifndef clox_value_h
 #define clox_value_h
 
+#include <string.h>
+
 #include "common.h"
 
 typedef struct Obj Obj;
 typedef struct ObjString ObjString;
+
+#ifdef NAN_BOXING
+
+// To speed up values, we use "boxed nans" so that a "quiet NaN" value can
+// represent a nil, true, false, or a pointer. This assumes that everything
+// above the 48th bit of a pointer is unused.
+
+#define SIGN_BIT ((uint64_t)0x8000000000000000)
+#define QNAN ((uint64_t)0x7ffc000000000000)
+
+#define TAG_NIL 1   // 0o01
+#define TAG_FALSE 2 // 0o10
+#define TAG_TRUE 3  // 0o11
+
+typedef uint64_t Value;
+
+#define IS_BOOL(value) (((value) | 1) == TRUE_VAL) // only use 'value' once!
+#define IS_NIL(value) ((value) == NIL_VAL)
+#define IS_NUMBER(value) (((value) & QNAN) != QNAN)
+#define IS_OBJ(value) (((value) & (QNAN | SIGN_BIT)) == (QNAN | SIGN_BIT))
+
+#define AS_BOOL(value) ((value) == TRUE_VAL)
+#define AS_NUMBER(value) valueToNum(value)
+#define AS_OBJ(value) ((Obj *)(uintptr_t)((value) & ~(SIGN_BIT | QNAN)))
+
+#define BOOL_VAL(b) ((b) ? TRUE_VAL : FALSE_VAL)
+#define FALSE_VAL ((Value)(uint64_t)(QNAN | TAG_FALSE))
+#define TRUE_VAL ((Value)(uint64_t)(QNAN | TAG_TRUE))
+#define NIL_VAL ((Value)(uint64_t)(QNAN | TAG_NIL))
+#define NUMBER_VAL(num) numToValue(num)
+#define OBJ_VAL(obj) (Value)(SIGN_BIT | QNAN | (uint64_t)(uintptr_t)(obj))
+
+// In theory, all these memcpy() calls can be optimised away.
+
+static inline double valueToNum(Value value)
+{
+    double num;
+    memcpy(&num, &value, sizeof(Value));
+    return num;
+}
+
+static inline Value numToValue(double num)
+{
+    Value value;
+    memcpy(&value, &num, sizeof(double));
+    return value;
+}
+
+#else
 
 typedef enum ValueType
 {
@@ -38,6 +89,8 @@ typedef struct Value
 #define NIL_VAL ((Value){VAL_NIL, {.number = 0}})
 #define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
 #define OBJ_VAL(object) ((Value){VAL_OBJ, {.obj = (Obj *)object}})
+
+#endif // NAN_BOXING
 
 typedef struct ValueArray
 {
